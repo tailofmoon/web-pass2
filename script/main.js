a998ba70e2b7f7109437e3cbf52f1e48fa22f32d
michael kwon <tailofmoon@naver.com>
2023-06-20 20:41:48 +0900

    탭영역 작업중

--- script/main.js
+++ script/main.js
@@ -9,4 +9,21 @@ $(function(){
             $(this).children('.depth2').removeClass('active');
         }
     );
+
+    // slider: fade 효과
+    let num = 0; // 이미지 번호
+
+    setInterval(function(){
+        num = num + 1; // 이미지 번호 증가
+        if(num > 2) num = 0;
+        console.log(num);
+        // 기존 이미지 제거
+        $('.slider .sliders a').removeClass('active');
+        // 현재 이미지 푯;
+        $('.slider .sliders a').eq(num).addClass('active');
+    }, 3000)
+
+
+
 });
+
