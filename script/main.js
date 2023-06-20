4f7b5e763cc7e3774a9cb83d54efe0bb827e40eb
michael kwon <tailofmoon@naver.com>
2023-06-20 21:22:34 +0900

    탭콘텐츠 구현

--- script/main.js
+++ script/main.js
@@ -19,11 +19,34 @@ $(function(){
         console.log(num);
         // 기존 이미지 제거
         $('.slider .sliders a').removeClass('active');
-        // 현재 이미지 푯;
+        // 현재 이미지 표시;
         $('.slider .sliders a').eq(num).addClass('active');
     }, 3000)
 
+    // tab
+    $('.tab-group > a:first-child').click(function(){
+        console.log('공지사항 버튼');
+        // 기본 버튼 스타일 제거
+        $('.tab-group > a').removeClass('active');
+        // 현재 버튼 active
+        $(this).addClass('active');
 
+        // 이전 콘텐츠 제거
+        $('.tab-content > div').removeClass('active');
+        // 현재 콘텐츠 표시
+        $('.tab-content > div:first-child').addClass('active');
+    })
+
+    $('.tab-group > a:last-child').click(function(){
+        console.log('갤러리 버튼');
+        $('.tab-group > a').removeClass('active');
+        $(this).addClass('active');
+
+        // 이전 콘텐츠 제거
+        $('.tab-content > div').removeClass('active');
+        // 현재 콘텐츠 표시
+        $('.tab-content > div:last-child').addClass('active');
+    })
 
 });
 
